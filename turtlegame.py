827d0c8abd8ebef7ce5b35abd80f27eb7be4eb9a
rajanterfa <49789975+rajanterfa@users.noreply.github.com>
2019-08-27 13:22:05 -0500

    Update turtlegame.py

--- turtlegame.py
+++ turtlegame.py
@@ -1,16 +1,3 @@
-# Rajan Terfa terfa003
-
-# I understand this is a graded, individual examination that may not be
-# discussed with anyone.  I also understand that obtaining solutions or
-# partial solutions from outside sources, or discussing
-# any aspect of the examination with anyone will result in failing the course.
-# I further certify that this program represents my own work and that none of
-# it was obtained from any source other than material presented as part of the
-# course.
-
-
-#Completed all the way through
-
 from turtle import *
 import tkinter.messagebox
 import tkinter
